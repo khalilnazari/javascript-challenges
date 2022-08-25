f3d3dfcef82825563ea58c92fd5d93eb035843ee
khalilnazari <khalil.deve@gmail.com>
2022-08-25 21:10:32 +0800

    wikipedia script is rectified

--- wikipedia-app/app.js
+++ wikipedia-app/app.js
@@ -5,28 +5,41 @@ const formDOM = document.querySelector('.form');
 const inputDOM = document.querySelector('.form-input');
 const resultsDOM = document.querySelector('.results');
 
+// submit form
 formDOM.addEventListener('submit', (e) => {
   e.preventDefault();
   const value = inputDOM.value;
+
+  // feild is empty 
   if (!value) {
     resultsDOM.innerHTML = `<div class='error'>please enter valid search term</div>`;
     return;
   }
-  fetchPages(value);
+
+  // fetch data
+  fetchData(value);
 });
 
-const fetchPages = async (searchValue) => {
+
+// fetchData
+const fetchData = async (searchValue) => {
+
+  // loading....
   resultsDOM.innerHTML = `<div class="loading"></div>`;
 
+  // try catch block 
   try {
     const response = await fetch(`${url}${searchValue}`);
     const data = await response.json();
     const results = data.query.search;
+
+    // if no result 
     if (results.length < 1) {
-      resultsDOM.innerHTML = `<div class='error'>no matching results. Please try again.</div>
-      `;
+      resultsDOM.innerHTML = `<div class='error'>no matching results. Please try again.</div>`;
       return;
     }
+
+    // display result
     renderResults(results);
   } catch (error) {
     console.log(error);
@@ -35,21 +48,15 @@ const fetchPages = async (searchValue) => {
   }
 };
 
+// renderResults
 const renderResults = (list) => {
-  const cardsList = list
-    .map((item) => {
-      const { title, snippet, pageid } = item;
-      return `<a href="http://en.wikipedia.org/?curid=${pageid}" target="_blank">
-            <h4>${title}</h4>
-            <p>
-              ${snippet}
-            </p>
-          </a>`;
-    })
-    .join('');
+  const cardsList = list.map((item) => {
+    const { title, snippet, pageid } = item;
+    return `<a href="http://en.wikipedia.org/?curid=${pageid}" target="_blank">
+              <h4>${title}</h4>
+              <p>${snippet}</p>
+            </a>`;
+  }).join('');
 
-  resultsDOM.innerHTML = `<div class="articles">
-          ${cardsList}
-        </div>
-      </div>`;
+  resultsDOM.innerHTML = `<div class="articles">${cardsList}</div></div>`;
 };
